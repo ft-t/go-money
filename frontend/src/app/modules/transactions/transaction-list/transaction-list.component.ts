c497936306324cb7390e87fbc0f88200141cff9b
Andrii <andrii@sotnikov.uk>
2025-03-03 18:48:53 +0200

    Feat: add mock list components

---
+++ frontend/src/app/modules/transactions/transaction-list/transaction-list.component.ts
@@ -0,0 +1,102 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import { BaseAutoUnsubscribeClass } from '../../../objects/auto-unsubscribe/base-auto-unsubscribe-class';
+import { BehaviorSubject, finalize } from 'rxjs';
+import { CurrenciesGrpcService } from '../../../services/currencies/currencies-grpc.service';
+import { tap } from 'rxjs/operators';
+import { GetCurrenciesResponse } from '../../../../../gen/gomoneypb/currency/v1/currency_pb';
+
+@Component({
+  selector: 'app-currency-list',
+  standalone: false,
+  templateUrl: 'currency-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CurrencyListComponent extends BaseAutoUnsubscribeClass implements OnInit, OnDestroy {
+  public currencies$ = new BehaviorSubject<any[] | any>([]);
+
+  public override isLoading$ = new BehaviorSubject<boolean>(false);
+
+  @ViewChild('filter') filter!: ElementRef;
+
+  constructor(private currenciesService: CurrenciesGrpcService) {
+    super();
+  }
+
+  override ngOnInit() {
+    super.ngOnInit();
+  }
+
+  override ngOnDestroy() {
+    super.ngOnDestroy();
+  }
+
+  getCurrencies() {
+
+    this.isLoading$.next(true);
+
+    const request = {
+    }
+
+    this.currenciesService.getCurrencies(request)
+      .pipe(
+        tap((response: GetCurrenciesResponse | any) => {
+          console.log(response);
+          this.currencies$.next(response.data);
+        }),
+        finalize(() => this.isLoading$.next(false)),
+        this.takeUntilDestroy
+      ).subscribe();
+  }
+
+  createCurrency() {
+    this.isLoading$.next(true);
+
+    const request = {
+
+    }
+
+    this.currenciesService.createCurrency(request)
+      .pipe(
+        tap(),
+        finalize(() => this.isLoading$.next(false)),
+        this.takeUntilDestroy
+      ).subscribe();
+  }
+
+  updateCurrency() {
+    this.isLoading$.next(true);
+
+    const request = {
+
+    }
+
+    this.currenciesService.updateCurrency(request)
+      .pipe(
+        tap(),
+        finalize(() => this.isLoading$.next(false)),
+        this.takeUntilDestroy
+      ).subscribe();
+  }
+
+  deleteCurrency(currency: any) {
+    this.isLoading$.next(true);
+
+    const request = {
+
+    }
+
+    this.currenciesService.deleteCurrency(request)
+      .pipe(
+        tap(),
+        finalize(() => this.isLoading$.next(false)),
+        this.takeUntilDestroy
+      ).subscribe();
+  }
+}
