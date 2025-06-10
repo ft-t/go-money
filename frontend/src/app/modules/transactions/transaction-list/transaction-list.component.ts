d10f17578cac47bd9e2f3bf4ce69e8c9d265bb8a
Stas Dm <iqpirat@gmail.com>
2025-06-10 17:13:47 +0200

    fix: build

--- frontend/src/app/modules/transactions/transaction-list/transaction-list.component.ts
+++ frontend/src/app/modules/transactions/transaction-list/transaction-list.component.ts
@@ -36,66 +36,66 @@ export class CurrencyListComponent extends BaseAutoUnsubscribeClass implements O
     super.ngOnDestroy();
   }
 
-  getCurrencies() {
-
-    this.isLoading$.next(true);
-
-    const request = {
-    }
-
-    this.currenciesService.getCurrencies(request)
-      .pipe(
-        tap((response: GetCurrenciesResponse | any) => {
-          console.log(response);
-          this.currencies$.next(response.data);
-        }),
-        finalize(() => this.isLoading$.next(false)),
-        this.takeUntilDestroy
-      ).subscribe();
-  }
-
-  createCurrency() {
-    this.isLoading$.next(true);
-
-    const request = {
-
-    }
-
-    this.currenciesService.createCurrency(request)
-      .pipe(
-        tap(),
-        finalize(() => this.isLoading$.next(false)),
-        this.takeUntilDestroy
-      ).subscribe();
-  }
-
-  updateCurrency() {
-    this.isLoading$.next(true);
-
-    const request = {
-
-    }
-
-    this.currenciesService.updateCurrency(request)
-      .pipe(
-        tap(),
-        finalize(() => this.isLoading$.next(false)),
-        this.takeUntilDestroy
-      ).subscribe();
-  }
-
-  deleteCurrency(currency: any) {
-    this.isLoading$.next(true);
-
-    const request = {
-
-    }
-
-    this.currenciesService.deleteCurrency(request)
-      .pipe(
-        tap(),
-        finalize(() => this.isLoading$.next(false)),
-        this.takeUntilDestroy
-      ).subscribe();
-  }
+  // getCurrencies() {
+  //
+  //   this.isLoading$.next(true);
+  //
+  //   const request = {
+  //   }
+  //
+  //   this.currenciesService.getCurrencies(request)
+  //     .pipe(
+  //       tap((response: GetCurrenciesResponse | any) => {
+  //         console.log(response);
+  //         this.currencies$.next(response.data);
+  //       }),
+  //       finalize(() => this.isLoading$.next(false)),
+  //       this.takeUntilDestroy
+  //     ).subscribe();
+  // }
+  //
+  // createCurrency() {
+  //   this.isLoading$.next(true);
+  //
+  //   const request = {
+  //
+  //   }
+  //
+  //   this.currenciesService.createCurrency(request)
+  //     .pipe(
+  //       tap(),
+  //       finalize(() => this.isLoading$.next(false)),
+  //       this.takeUntilDestroy
+  //     ).subscribe();
+  // }
+  //
+  // updateCurrency() {
+  //   this.isLoading$.next(true);
+  //
+  //   const request = {
+  //
+  //   }
+  //
+  //   this.currenciesService.updateCurrency(request)
+  //     .pipe(
+  //       tap(),
+  //       finalize(() => this.isLoading$.next(false)),
+  //       this.takeUntilDestroy
+  //     ).subscribe();
+  // }
+  //
+  // deleteCurrency(currency: any) {
+  //   this.isLoading$.next(true);
+  //
+  //   const request = {
+  //
+  //   }
+  //
+  //   this.currenciesService.deleteCurrency(request)
+  //     .pipe(
+  //       tap(),
+  //       finalize(() => this.isLoading$.next(false)),
+  //       this.takeUntilDestroy
+  //     ).subscribe();
+  // }
 }
