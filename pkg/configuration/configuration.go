18e02bc4668023932422659da3b2a06add92fd7c
Stanislav Dmytryshyn <iqpirat@gmail.com>
2025-06-22 01:11:43 +0200

    fix: ci db

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -2,7 +2,10 @@ package configuration
 
 import (
 	"context"
+	"fmt"
+	"github.com/ft-t/go-money/pkg/boilerplate"
 	"github.com/sethvargo/go-envconfig"
+	"os"
 )
 
 var configuration *Configuration
@@ -11,13 +14,18 @@ func GetConfiguration() *Configuration {
 	if configuration != nil {
 		return configuration
 	}
-	
+
 	var cfg Configuration
 
 	if err := envconfig.ProcessWith(context.Background(), &envconfig.Config{
 		Target: &cfg,
 	}); err != nil {
 		panic(err)
+	}
+
+	if boilerplate.GetCurrentEnvironment() == boilerplate.Ci { // for CI, we use a unique database name to avoid conflicts
+		configuration.Db.Db = fmt.Sprintf("ci_%v", int64(os.Getpid()))
+		configuration.ReadOnlyDb.Db = configuration.Db.Db
 	}
 
 	configuration = &cfg
