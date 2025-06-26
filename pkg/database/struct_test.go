6a1a536ef7da787e535c90402ee91481c5337180
Stanislav Dmytryshyn <iqpirat@gmail.com>
2025-06-26 11:57:48 +0200

    test: improve coverage

---
+++ pkg/database/struct_test.go
@@ -0,0 +1,34 @@
+package database_test
+
+import (
+	"github.com/ft-t/go-money/pkg/database"
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStruct(t *testing.T) {
+	testCases := []struct {
+		val      string
+		expected string
+	}{
+		{
+			val:      lo.ToPtr(database.Currency{}).TableName(),
+			expected: "currencies",
+		},
+		{
+			val:      lo.ToPtr(database.Tag{}).TableName(),
+			expected: "tags",
+		},
+		{
+			val:      lo.ToPtr(database.ImportDeduplication{}).TableName(),
+			expected: "import_deduplication",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.val, func(t *testing.T) {
+			assert.EqualValues(t, tc.expected, tc.val)
+		})
+	}
+}
