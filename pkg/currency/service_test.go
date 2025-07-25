5fc60c9f8c745e85eed0edcd2759785ccf98b727
Stanislav Dmytryshyn <iqpirat@gmail.com>
2025-07-25 13:06:01 +0200

    test: improve cov

--- pkg/currency/service_test.go
+++ pkg/currency/service_test.go
@@ -4,6 +4,7 @@ import (
 	currencyv1 "buf.build/gen/go/xskydev/go-money-pb/protocolbuffers/go/gomoneypb/currency/v1"
 	v1 "buf.build/gen/go/xskydev/go-money-pb/protocolbuffers/go/gomoneypb/v1"
 	"context"
+	"github.com/cockroachdb/errors"
 	"github.com/ft-t/go-money/pkg/currency"
 	"github.com/ft-t/go-money/pkg/database"
 	"github.com/ft-t/go-money/pkg/testingutils"
@@ -60,6 +61,43 @@ func TestGetCurrencies(t *testing.T) {
 		assert.Equal(t, "GBP", resp.Currencies[2].Id)
 	})
 
+	t.Run("success", func(t *testing.T) {
+		assert.NoError(t, testingutils.FlushAllTables(cfg.Db))
+
+		cur1 := &database.Currency{
+			ID:            "USD",
+			Rate:          decimal.NewFromInt(2),
+			IsActive:      true,
+			DecimalPlaces: 4,
+		}
+		cur2 := &database.Currency{
+			ID:       "EUR",
+			Rate:     decimal.NewFromInt(3),
+			IsActive: true,
+		}
+		cur3 := &database.Currency{
+			ID:   "GBP",
+			Rate: decimal.NewFromInt(4),
+			DeletedAt: gorm.DeletedAt{
+				Time:  time.Now(),
+				Valid: true,
+			},
+		}
+
+		assert.NoError(t, gormDB.Create(cur1).Error)
+		assert.NoError(t, gormDB.Create(cur2).Error)
+		assert.NoError(t, gormDB.Create(cur3).Error)
+
+		srv := currency.NewService()
+
+		resp, err := srv.GetCurrencies(context.TODO(), &currencyv1.GetCurrenciesRequest{
+			IncludeDisabled: false,
+		})
+		assert.NoError(t, err)
+
+		assert.Len(t, resp.Currencies, 2)
+	})
+
 	t.Run("filter by id", func(t *testing.T) {
 		assert.NoError(t, testingutils.FlushAllTables(cfg.Db))
 
@@ -102,6 +140,25 @@ func TestGetCurrencies(t *testing.T) {
 
 		assert.Equal(t, "EUR", resp.Currencies[0].Id)
 		assert.Equal(t, "GBP", resp.Currencies[1].Id)
+	})
+
+	t.Run("find err", func(t *testing.T) {
+		mockGorm, _, sql := testingutils.GormMock()
+		ctx := database.WithContext(context.TODO(), mockGorm)
+
+		srv := currency.NewService()
+
+		sql.ExpectQuery("SELECT .*").WillReturnError(errors.New("unexpected err"))
+
+		resp, err := srv.GetCurrencies(ctx, &currencyv1.GetCurrenciesRequest{
+			Ids: []string{
+				"EUR",
+				"GBP",
+			},
+			IncludeDisabled: true,
+		})
+		assert.ErrorContains(t, err, "unexpected err")
+		assert.Nil(t, resp)
 	})
 }
 
