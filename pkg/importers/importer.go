89b5b5bf8c5a9b8102946bdf6a9f11de7652b43e
Stanislav Dmytryshyn <iqpirat@gmail.com>
2026-04-20 14:18:23 +0200

    feat(importers): tag imported transactions with IMPORTER actor

--- pkg/importers/importer.go
+++ pkg/importers/importer.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ft-t/go-money/pkg/boilerplate"
 	"github.com/ft-t/go-money/pkg/database"
 	"github.com/ft-t/go-money/pkg/transactions"
+	"github.com/ft-t/go-money/pkg/transactions/history"
 	"github.com/google/uuid"
 	"github.com/lib/pq"
 	"github.com/rs/zerolog/log"
@@ -152,6 +153,8 @@ func (i *Importer) Import(
 			Req: item.CreateRequest,
 		})
 	}
+
+	ctx = history.WithActor(ctx, history.ImporterActor(importerSourceName(req.Source)))
 
 	tx := database.FromContext(ctx, database.GetDb(database.DbTypeMaster)).Begin()
 	defer tx.Rollback()
@@ -326,4 +329,21 @@ func (i *Importer) ConvertRequestsToTransactions(
 	}
 
 	return result, nil
+}
+
+func importerSourceName(src importv1.ImportSource) string {
+	switch src {
+	case importv1.ImportSource_IMPORT_SOURCE_FIREFLY:
+		return "firefly"
+	case importv1.ImportSource_IMPORT_SOURCE_PRIVATE_24:
+		return "privat24"
+	case importv1.ImportSource_IMPORT_SOURCE_REVOLUT:
+		return "revolut"
+	case importv1.ImportSource_IMPORT_SOURCE_MONOBANK:
+		return "monobank"
+	case importv1.ImportSource_IMPORT_SOURCE_BNP_PARIBAS_POLSKA:
+		return "paribas"
+	default:
+		return "unknown"
+	}
 }
